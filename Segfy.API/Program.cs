2654bdcd9dbadd43fba2ac059f028df9e737c5f2
Endriw Ventura <endriwventura@yahoo.com>
2026-07-05 15:53:46 -0300

    Feat: Aplicar alterações de migrtions automaticamente ao iniciar o projeto em dev

--- Segfy.API/Program.cs
+++ Segfy.API/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.EntityFrameworkCore;
 using Segfy.Application.DependencyInjection;
 using Segfy.Infrastructure.DependencyInjection;
+using Segfy.Infrastructure.Persistence.Context;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +19,22 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    using var scope = app.Services.CreateScope();
+
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+    try
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDBContext>();
+        dbContext.Database.Migrate();
+
+        logger.LogInformation("Banco de dados atualizado com sucesso.");
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex, "Erro ao aplicar as migrations.");
+        throw;
+    }
 }
 
 app.UseHttpsRedirection();
